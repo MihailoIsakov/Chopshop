01cccfc2228a32a5c9fe58860c39858ed6df2d34
Zieghailo <isakov.m@gmail.com>
2015-03-18 20:06:22 +0100

    Working background video

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    $("#video-wallpaper").wallpaper({
+        source: {
+            poster: "img/python.png",
+            video: "http://www.youtube.com/watch?v=AiCFtmdrvHM"
+        }
+    });
+});
